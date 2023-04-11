40dba201f4798d502ef73f22fea9953284f85687
javajoker0919 <javajoker919@gmail.com>
2023-04-11 12:31:33 -0400

    update password functionality has been completed.

--- routes/api/index.js
+++ routes/api/index.js
@@ -1,14 +1,26 @@
 const express = require("express");
 const router = express.Router();
 
-const { getOrdersController } = require('../../controllers/api/getOrdersController');
-const { getOrdersByLocationController } = require('../../controllers/api/getOrdersByLocationController');
-const { getLocationsController } = require("../../controllers/api/getLocationsController");
-const { getAcessByPasswordController } = require("../../controllers/api/getAcessByPasswordController");
+const {
+  getOrdersController,
+} = require("../../controllers/api/getOrdersController");
+const {
+  getOrdersByLocationController,
+} = require("../../controllers/api/getOrdersByLocationController");
+const {
+  getLocationsController,
+} = require("../../controllers/api/getLocationsController");
+const {
+  getAcessByPasswordController,
+} = require("../../controllers/api/getAcessByPasswordController");
+const {
+  updatePasswordController,
+} = require("../../controllers/api/updatePasswordController");
 
-router.get('/orders', getOrdersController);
-router.post('/getAccessByPassword', getAcessByPasswordController);
-router.post('/ordersByLocation', getOrdersByLocationController);
-router.get('/locations', getLocationsController);
+router.get("/orders", getOrdersController);
+router.post("/getAccessByPassword", getAcessByPasswordController);
+router.post("/ordersByLocation", getOrdersByLocationController);
+router.get("/locations", getLocationsController);
+router.post("/updatePassword", updatePasswordController);
 
 module.exports = router;
