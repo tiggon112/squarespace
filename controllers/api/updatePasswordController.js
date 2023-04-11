aff33f6d0a1554a3e682226bb7692f51652b9782
javajoker0919 <javajoker919@gmail.com>
2023-04-11 15:51:06 -0400

    Login functionality added.

--- controllers/api/updatePasswordController.js
+++ controllers/api/updatePasswordController.js
@@ -4,18 +4,25 @@ exports.updatePasswordController = (req, res) => {
   console.log("req : ", req);
   const password = req.body.updatedPassword;
   const page = req.body.location;
+  const username = req.body.username;
   const encrypted_password = CryptoJS.AES.encrypt(password, page);
 
-  const get_sql =
+  let get_sql =
     "UPDATE passwords SET password = '" +
     encrypted_password +
     "' WHERE page='" +
     page +
     "'";
 
+  if (page == "login") {
+    get_sql += " AND username = '" + username + "'";
+  }
+
   req.db.query(get_sql, (err, result) => {
     if (result.affectedRows == 1) {
       res.json("success");
+    } else {
+      res.json("failed");
     }
   });
 };
