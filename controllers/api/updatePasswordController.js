7b82984fe2528423914069db40e8a81263304d1f
tiggon112 <tiggon112@gmail.com>
2023-04-12 15:21:27 +0000

    really final

--- controllers/api/updatePasswordController.js
+++ controllers/api/updatePasswordController.js
@@ -2,9 +2,11 @@ const CryptoJS = require("crypto-js");
 
 exports.updatePasswordController = (req, res) => {
   const password = req.body.updatedPassword;
-  const page = req.body.location;
-  const username = req.body.username;
-  const encrypted_password = CryptoJS.AES.encrypt(password, page);
+  // const page = req.body.location;
+  const page = "admin login";
+  const keyString ="Entrepreneurscamps";
+  // const username = req.body.username;
+  const encrypted_password = CryptoJS.AES.encrypt(password, keyString);
 
   let get_sql =
     "UPDATE passwords SET password = '" +
@@ -13,9 +15,9 @@ exports.updatePasswordController = (req, res) => {
     page +
     "'";
 
-  if (page == "login") {
-    get_sql += " AND username = '" + username + "'";
-  }
+  // if (page == "login") {
+    // get_sql += " AND username = '" + username + "'";
+  // }
 
   req.db.query(get_sql, (err, result) => {
     if (result.affectedRows == 1) {
